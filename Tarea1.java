cfc23bebbc1e312d06e4ec1c8b6f812a20d85f53
dcaguilarc <dcaguilarc@uce.edu.ec>
2024-11-08 15:27:44 -0500

    Positive or Negative

--- Tarea1.java
+++ Tarea1.java
@@ -11,9 +11,9 @@ public class Tarea1 {
 
         // Determinar si el número es positivo, negativo o cero
         if (numero > 0) {
-            System.out.println("El número es positivo.");
+            System.out.println("El número es positivo +.");
         } else if (numero < 0) {
-            System.out.println("El número es negativo.");
+            System.out.println("El número es negativo -.");
         } else {
             System.out.println("El número es cero.");
         }
